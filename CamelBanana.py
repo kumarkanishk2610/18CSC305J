fc089b4b9c7ffe87e637eaac720f19b2e5970b69
Kanishk Kumar <104393489+kumarkanishk2610@users.noreply.github.com>
2024-05-09 00:09:52 +0530

    Updated CamelBanana, MonteHall

--- CamelBanana.py
+++ CamelBanana.py
@@ -1,33 +1,24 @@
 dp = [[-1 for i in range(3001)] for j in range(1001)]
-
-def recBananaCnt(A, B, C):
+def main(A, B, C):
 	if B <= A:
 		return 0
-		
 	if B <= C:
 		return B - A
-	
-	if A == 0:
-		return B
-	
 	if dp[A][B] != -1:
 		return dp[A][B]
-	
-	maxCount = -2**32
-	tripCount = ((2 * B) // C) - 1 if(B % C == 0 ) else ((2 * B) // C) + 1
-
+	maxCount = 0
+	x=2*B//C
+	if B % C==0:
+		x = x-1
+	else:
+		x = x+1
 	for i in range(1,A+1):
-		curCount = recBananaCnt(A - i, B - tripCount * i, C)
+		curCount = main(A - i, B - x * i, C)
 		if curCount > maxCount:
 			maxCount = curCount
 			dp[A][B] = maxCount
-		
 	return maxCount
-
-def maxBananaCnt(A, B, C):
-	return recBananaCnt(A, B, C)
-
 A = 1000
 B = 3000
 C = 1000
-print(maxBananaCnt(A, B, C))
+print(main(A, B, C))
--- MonteHall.py
+++ MonteHall.py
@@ -1,14 +1,15 @@
 import random
-def monty_hall_simulations(n):
-    wins = 0
-    for _ in range(n):
-        prize_door = random.randint(1, 3)
-        chosen_door = random.randint(1, 3)
-        revealed_door = random.choice([door for door in range(1, 4) if door != chosen_door and door != prize_door])
-        chosen_door = [door for door in range(1, 4) if door != chosen_door and door != revealed_door][0]
-        if chosen_door == prize_door:
-            wins += 1
-    return wins / n
-n = int(input("Enter number of simulations: "))
-average_probability = monty_hall_simulations(n)
-print("Average probability of winning if switching doors:", average_probability)
+def main(n):
+    wins=0
+    for i in range (n):
+        prizedoor=random.randint(1,3)
+        chosendoor=random.randint(1,3)
+        revealeddoor=random.randint(1,3)
+        while(revealeddoor!=chosendoor and revealeddoor!=prizedoor):
+            revealeddoor=random.randint(1,3)
+        while(chosendoor!=revealeddoor):
+            chosendoor=random.randint(1,3)
+        if chosendoor==prizedoor:
+            wins+=1
+    print (wins/n)
+main(100000)
